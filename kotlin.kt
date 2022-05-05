619498e3ff1a4d53fed3a3f8868680f7b6ab5ef2
Haluk D <76127756+halukdundar@users.noreply.github.com>
2022-05-05 23:01:16 +0300

    Update kotlin.kt

--- kotlin.kt
+++ kotlin.kt
@@ -1,3 +1,12 @@
+/*
+
+Kapsülleme (Encapsulation)
+OOP--Nesne yönelimli programlamanın ilk prensibi kapsülleme (encapsulation) olarak adlandırılmaktadır.
+Oluşturulan bir sınıf (class) içerisinde kullanıcının işlemlerini daha kolay gerçekleştirebilmesi 
+için bazı işlemler birleştirilerek tek bir işlem gibi gösterilir. 
+
+Nesneyeye hangi türden erişimlerin olacağını gösterir.
+*/
 //Encapsulation
 
 class Sanatci(isim:String, yas: Int, meslek:String, uzmanlik:String){
