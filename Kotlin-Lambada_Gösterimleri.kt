65b338b08cfb97b7cba6103c4260ff59c83b0fae
Haluk D <76127756+halukdundar@users.noreply.github.com>
2022-05-06 09:34:11 +0300

    Update Kotlin-Lambada_Gösterimleri.kt

--- Kotlin-Lambada_Gösterimleri.kt
+++ Kotlin-Lambada_Gösterimleri.kt
@@ -1,9 +1,8 @@
 //Kotlin Lambda Kullanımı ---- Lambada Gösterimi bu şekildedir (   ->    )
-/*
 
-
-
-
-*/
 // TOPLAMA fonksiyonu ve geriye iki sayının toplamını veren lambada gösterimiyle 
+
 val toplama = { x: Int, y: Int -> x + y }
+
+println(toplama(11,23)
+
