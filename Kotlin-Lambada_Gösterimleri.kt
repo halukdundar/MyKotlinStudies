b3ee07e5edf2f223020fc45a9e4c78cf4628706e
Haluk D <76127756+halukdundar@users.noreply.github.com>
2022-05-06 09:39:23 +0300

    Update Kotlin-Lambada_Gösterimleri.kt

--- Kotlin-Lambada_Gösterimleri.kt
+++ Kotlin-Lambada_Gösterimleri.kt
@@ -2,7 +2,18 @@
 
 // TOPLAMA fonksiyonu ve geriye iki sayının toplamını veren lambada gösterimiyle 
 
+
 val toplama = { x: Int, y: Int -> x + y }
 
-println(toplama(11,23)
+println(toplama(11,23) // geriye 34 değerini vermektedir.
 
+
+        
+
+        
+//Diğer bir örnek gösterim
+
+val carpma: (Int,Int)-> Int={x,y-> x*y}
+
+
+println(carpma(11,11))  //geriye 121 değerini vermektedir.
